1bf6ccb8a97d55fa8c3d9b5d8ae1b4de29abd3cd
izabela kopczynska <izabela.anna.k@gmail.com>
2018-03-20 19:13:56 +0100

    Adding Testing Methods About Camera.

--- src/main/java/com/demo/camera/PhotoCamera.java
+++ src/main/java/com/demo/camera/PhotoCamera.java
@@ -2,16 +2,35 @@ package com.demo.camera;
 
 public class PhotoCamera {
 
+    private ImageSensor imageSensor;
+    private boolean isTurnedOn;
+
+    public PhotoCamera(ImageSensor imageSensor) {
+        this.imageSensor = imageSensor;
+        isTurnedOn = false;
+    }
+
     public void turnOn() {
-        // not implemented
+        isTurnedOn = true;
+        imageSensor.turnOn();
     }
 
     public void turnOff() {
-        // not implemented
+        isTurnedOn = false;
+        imageSensor.turnOff();
     }
 
     public void pressButton() {
-        // not implemented
+        if(!isTurnedOn){
+            //donothing.
+        } else {
+            imageSensor.read();
+            try {
+                Thread.sleep(300);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
 
--- src/test/java/com/demo/camera/PhotoCameraTest.java
+++ src/test/java/com/demo/camera/PhotoCameraTest.java
@@ -1,14 +1,60 @@
 package com.demo.camera;
 
 import org.assertj.core.api.Assertions;
+import org.junit.Assert;
 import org.junit.Test;
+import org.mockito.Mockito;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.mockitoSession;
 
 public class PhotoCameraTest {
 
 
     @Test
-    public void changeName() {
-        Assertions.assertThat(this).isNotNull();
+    public void whenCameraIsTurnedOnTheSensorIsTurningOn(){
+        ImageSensor imageSensor = mock(ImageSensor.class);
+        PhotoCamera photoCamera = new PhotoCamera(imageSensor);
+        photoCamera.turnOn();
+        Mockito.verify(imageSensor).turnOn();
     }
 
+    @Test
+    public void whenCameraIsTurnedOffTheSensorIsTurnedOff(){
+        ImageSensor imageSensor = mock(ImageSensor.class);
+        PhotoCamera photoCamera = new PhotoCamera(imageSensor);
+        photoCamera.turnOff();
+        Mockito.verify(imageSensor).turnOff();
+    }
+
+    @Test
+    public void whenCameraIsTurnedOffTheButtonDoesNothing(){
+        ImageSensor imageSensor = mock(ImageSensor.class);
+        PhotoCamera photoCamera = new PhotoCamera (imageSensor);
+        photoCamera.pressButton();
+
+        Mockito.verifyZeroInteractions(imageSensor);
+    }
+
+
+    @Test
+    public void whenCameraIsTurnedOnSensorSendsData(){
+        ImageSensor imageSensor = mock(ImageSensor.class);
+        PhotoCamera photoCamera = new PhotoCamera (imageSensor);
+        photoCamera.turnOn();
+        photoCamera.pressButton();
+
+        Mockito.verify(imageSensor).read();
+    }
+
+    @Test
+    public void whenDataIsCurrentlySavingTurningOffTheCameraDontTurnOffTheSensor(){
+        ImageSensor imageSensor = mock(ImageSensor.class);
+        PhotoCamera photoCamera = new PhotoCamera (imageSensor);
+        photoCamera.turnOn();
+        photoCamera.pressButton();
+        photoCamera.turnOff();
+
+        
+    }
 }
